ee3e9c9a328b3f3c0f2521a6a17db746854413f1
sinmetal <sinmetal@sinmetalcraft.jp>
2025-05-01 18:07:29 +0900

    Rollback flag (#142)
    * Revert "Fix required argument handling (#141)"
    
    This reverts commit 3b4b3f9bc7e8af15a9ced2b3b7743d73a6e8707f.
    
    * Revert "make directory flag required (#137)"
    
    This reverts commit 3c0e8bf7e0b5d5ffebecc9786e1ffadfc8fbe332.

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,6 @@ package cmd
 import (
 	"context"
 	"io/fs"
-	"log"
 	"os"
 	"runtime/debug"
 	"time"
@@ -60,6 +59,7 @@ var rootCmd = &cobra.Command{
 			ctx = wrenchfs.WithContext(ctx, CustomFileSystemFunc())
 			cmd.SetContext(ctx)
 		}
+
 		return nil
 	},
 }
@@ -83,22 +83,16 @@ func init() {
 	rootCmd.AddCommand(truncateCmd)
 	rootCmd.AddCommand(instanceCmd)
 
-	rootCmd.PersistentFlags().StringVar(&project, flagNameProject, spannerProjectID(), "GCP project id. If not set, will use $SPANNER_PROJECT_ID or $GOOGLE_CLOUD_PROJECT value")
-	rootCmd.PersistentFlags().StringVar(&instance, flagNameInstance, spannerInstanceID(), "Cloud Spanner instance name. If not set, will use $SPANNER_INSTANCE_ID value")
-	rootCmd.PersistentFlags().StringVar(&database, flagNameDatabase, spannerDatabaseID(), "Cloud Spanner database name If not set, will use $SPANNER_DATABASE_ID value")
-	rootCmd.PersistentFlags().StringVar(&directory, flagNameDirectory, "", "Directory that schema file placed")
-	rootCmd.PersistentFlags().StringVar(&schemaFile, flagNameSchemaFile, "", "Name of schema file If not set, will use default 'schema.sql' file name")
+	rootCmd.PersistentFlags().StringVar(&project, flagNameProject, spannerProjectID(), "GCP project id (optional. if not set, will use $SPANNER_PROJECT_ID or $GOOGLE_CLOUD_PROJECT value)")
+	rootCmd.PersistentFlags().StringVar(&instance, flagNameInstance, spannerInstanceID(), "Cloud Spanner instance name (optional. if not set, will use $SPANNER_INSTANCE_ID value)")
+	rootCmd.PersistentFlags().StringVar(&database, flagNameDatabase, spannerDatabaseID(), "Cloud Spanner database name (optional. if not set, will use $SPANNER_DATABASE_ID value)")
+	rootCmd.PersistentFlags().StringVar(&directory, flagNameDirectory, "", "Directory that schema file placed (required)")
+	rootCmd.PersistentFlags().StringVar(&schemaFile, flagNameSchemaFile, "", "Name of schema file (optional. if not set, will use default 'schema.sql' file name)")
 	rootCmd.PersistentFlags().StringVar(&credentialsFile, flagCredentialsFile, "", "Specify Credentials File")
 	rootCmd.PersistentFlags().DurationVar(&timeout, flagTimeout, time.Hour, "Context timeout")
 
 	rootCmd.Version = versionInfo()
 	rootCmd.SetVersionTemplate(versionTemplate)
-
-	if CustomFileSystemFunc == nil {
-		if err := cobra.MarkFlagRequired(rootCmd.PersistentFlags(), flagNameDirectory); err != nil {
-			log.Fatal(err)
-		}
-	}
 }
 
 func spannerProjectID() string {
